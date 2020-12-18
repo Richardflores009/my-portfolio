bc5515ac532bc7d09e7aa202ab8845149d0d7665
Richy Rich <richardflores009@gmail.com>
2020-12-17 21:39:40 -0700

    big chunk got lost in the logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
+import Project from './components/Project';
+import AboutMe from './components/AboutMe'
+import Contact from './components/Contact'
+
 
 function App() {
+  const [currentPage, handlePageChange] = useState('About Me');
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'About Me':
+        return <AboutMe />;
+      case 'Project':
+        return <Project />;
+      case 'Contact':
+        return <Contact />;
+      default:
+        return <AboutMe />;
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+       
+      <main>
+      <div>{renderPage(currentPage)}</div>
+      </main>
+      <footer>
+      <Footer/>
+      </footer>
+      
     </div>
   );
 }
+
 
 export default App;
