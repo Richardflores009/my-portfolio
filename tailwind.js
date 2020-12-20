0aa3638f91476175b53118dbcc9168d145d889e9
Richy Rich <richardflores009@gmail.com>
2020-12-20 14:53:57 -0700

    added some responsivness

--- tailwind.js
+++ tailwind.js
@@ -6,8 +6,8 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      sm: '640px',
-      md: '768px',
+      sm: '1037px',
+      md: '800px',
       lg: '1024px',
       xl: '1280px',
       '2xl': '1536px',
