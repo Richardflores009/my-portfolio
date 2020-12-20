39006b51820474e9144226c96e8fd262c57f4ba6
Richy Rich <richardflores009@gmail.com>
2020-12-20 10:42:07 -0700

    added copy pasta about me info from some random article

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -7,14 +7,9 @@ const AboutMe = () => (
     <h1 className="font-sans text-6xl pb-10" >About Me</h1>
     <img  className="mx-auto pb-10" src={selfie} alt="Project"height="200" width="170" />
     <p className="font-sans text-center text-lg" >
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
-      varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
-      Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
-      imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum ligula
-      in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare elit.
-      Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
-      consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-      fames ac ante ipsum primis in faucibus.
+    Richard Flores is a Software Developer , with experience building applications for Front End and Back End projects. His background in managing large scale event production, band management, and many software development proficiencies help him leverage a collabrotive and fast paced approach. Richard is fueled by her passion for understanding the nuances of how software works. He considers gimself a ‘forever student,’ eager to both build on his academic foundations in Back End and Front End development and stay in tune with the latest digital technologies  through continued coursework.
+    His hunger for knowledge and determination to turn information into action has contributed to his most recent successes, where he built many collabrative projects and many on his own. Meanwhile, he vastly improved his productivity by asking questions, commenting code and keeping an organizied code base. 
+    Richard believes mindfulness in the workplace is key to success - a tenet he lives out through his interests in yoga, meditation, photography, and playing guitar. Richard is currently working as a freelance video director/audio engineer and is always interested in a challenge. Reach out to Richardflores009@gmail.com to connect!
     </p>
   </div>
   
